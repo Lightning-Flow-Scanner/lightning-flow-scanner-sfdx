827138cf0b1d01389ee45ecd2f4e840178c8b06a
Ruben <halmanrf@gmail.com>
2021-06-24 21:03:40 +0200

    init

---
+++ src/commands/hello/lint.ts
@@ -0,0 +1,51 @@
+import { flags, SfdxCommand } from '@salesforce/command';
+import { Messages, SfdxError } from '@salesforce/core';
+import { AnyJson } from '@salesforce/ts-types';
+import * as core from 'flowhealthcheck--core';
+
+// Initialize Messages with the current plugin directory
+Messages.importMessagesDirectory(__dirname);
+
+// Load the specific messages for this file. Messages from @salesforce/command, @salesforce/core,
+// or any library that is using the messages framework can also be loaded this way.
+const messages = Messages.loadMessages('flowhealthcheck-cli', 'command');
+
+export default class Lint extends SfdxCommand {
+
+  public static description = messages.getMessage('commandDescription');
+
+  public static examples = [
+    `$ sfdx hello:org --targetusername myOrg@example.com --targetdevhubusername devhub@org.com
+  Hello world! This is org: MyOrg and I will be around until Tue Mar 20 2018!
+  My hub org id is: 00Dxx000000001234
+  `];
+
+  // protected static flagsConfig = {
+    // flag with a value (-n, --name=VALUE)
+    // name: flags.string({char: 'n', description: messages.getMessage('nameFlagDescription')})
+    // force: flags.boolean({char: 'f', description: messages.getMessage('forceFlagDescription')})
+  // };
+
+  // Comment this out if your command does not require an org username
+  protected static requiresUsername = false;
+
+  // Comment this out if your command does not support a hub org username
+  protected static supportsDevhubUsername = true;
+
+  // Set this to true if your command requires a project workspace; 'requiresProject' is false by default
+  protected static requiresProject = true;
+
+  public async run(): Promise<AnyJson> {
+
+    // this.org is guaranteed because requiresUsername=true, as opposed to supportsUsername
+    // todo get latest version of all flows
+
+    const outputString = 'Hello ' + core.scan();
+    this.ux.log(outputString);
+    // todo use project
+    // this.project.
+
+    // Return an object to be displayed with --json
+    return { outputString };
+  }
+}
