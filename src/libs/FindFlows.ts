e9da0ac10e2f8e1eec56edd9a6f87c8fede3b390
Ruben <halmanrf@gmail.com>
2021-09-13 23:38:30 +0200

    use glob

--- src/libs/FindFlows.ts
+++ src/libs/FindFlows.ts
@@ -1,27 +1,10 @@
-import {readdirSync, statSync} from 'fs';
-import * as path from 'path';
+import {glob} from 'glob';
 
 export function FindFlows(dir: string) {
-  const allFiles = (dir, ext, files?, result?, regex?) => {
-    files = files || readdirSync(dir);
-    result = result || [];
-    regex = regex || new RegExp(`\\${ext}$`);
 
-    for (let i = 0; i < files.length; i++) {
-      let file = path.join(dir, files[i]);
-      if (statSync(file).isDirectory() && !path.extname(file)) {
-        try {
-          result = allFiles(file, ext, readdirSync(file), result, regex);
-        } catch (error) {
-          continue;
-        }
-      } else {
-        if (regex.test(file)) {
-          result.push(file);
-        }
-      }
-    }
-    return result;
+  const getDirectories = function (src) {
+    return glob.sync(src + '/**/*.flow-meta.xml');
   };
-  return allFiles(dir, '.flow-meta.xml');
+  return getDirectories(dir);
+
 }
