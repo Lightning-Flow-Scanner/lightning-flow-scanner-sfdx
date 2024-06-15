9ef956f3fbcd7ed630344a589f6480253d01ef79
junners <jose.junryan@gmail.com>
2024-06-14 18:54:08 -0700

    chore: pin package updates

--- releaseconfig/release.config.mjs
+++ releaseconfig/release.config.mjs
@@ -26,5 +26,12 @@ export default {
         ],
       },
     ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json", "CHANGELOG.md"],
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
   ]
 };
