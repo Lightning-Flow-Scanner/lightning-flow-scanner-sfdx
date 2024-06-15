839a2ab32db15d64020ad628126b9f6eab5129b2
junners <jose.junryan@gmail.com>
2024-06-14 17:54:01 -0700

    chore: init release configurations

---
+++ release.config.mjs
@@ -0,0 +1,20 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+  branches: [
+    {
+      name: "master",
+      channel: "beta"
+    }
+  ],
+  plugins: [
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json", "CHANGELOG.md"],
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ]
+}
