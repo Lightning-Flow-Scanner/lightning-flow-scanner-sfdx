d3f07e4262d34644e6d4572cec3821d73e1be4a7
junners <jose.junryan@gmail.com>
2024-06-14 18:35:10 -0700

    chore: add commit analyzer for consistent release

--- release.config.mjs
+++ release.config.mjs
@@ -7,5 +7,24 @@ export default {
       name: "master",
       channel: "beta"
     }
+  ],
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "conventionalcommits",
+        releaseRules: [
+          { type: "fix", release: "patch" },
+          { type: "feat", release: "patch" },
+          { type: "build", release: "patch" },
+          { type: "chore", release: "patch" },
+          { type: "docs", release: "patch" },
+          { type: "style", release: "patch" },
+          { type: "refactor", release: "patch" },
+          { type: "perf", release: "patch" },
+          { type: "test", release: "patch" },
+        ],
+      },
+    ],
   ]
 };
