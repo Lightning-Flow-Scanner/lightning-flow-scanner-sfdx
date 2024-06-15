0a3002b6480b2fdd919f18f5ac5804787c42d8d6
junners <jose.junryan@gmail.com>
2024-06-14 18:23:51 -0700

    chore: remove semantic on git

--- release.config.mjs
+++ release.config.mjs
@@ -7,14 +7,5 @@ export default {
       name: "master",
       channel: "beta"
     }
-  ],
-  plugins: [
-    [
-      "@semantic-release/git",
-      {
-        assets: ["package.json", "CHANGELOG.md"],
-        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-      },
-    ],
   ]
 };
