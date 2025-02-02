4b57be6dc019a1e70a81b5206bcaeee02f29248a
junners <jose.junryan@gmail.com>
2025-02-01 20:12:52 -0800

    chore: modify npm scripts to use base semantic release config

--- release.config.mjs
+++ release.config.mjs
@@ -30,7 +30,7 @@ export default {
     [
       "@semantic-release/git",
       {
-        assets: ["package.json", "CHANGELOG.md"],
+        assets: ["package.json", "package-lock.json", "CHANGELOG.md"],
         message:
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
